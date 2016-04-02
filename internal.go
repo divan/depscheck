00650e91e9f21b9d859fbb8adbf563d291bc9acf
Andrew Pogrebnoy <absourd.noise@gmail.com>
2016-04-03 00:19:13 +0300

    issue#7 go fmt

--- internal.go
+++ internal.go
@@ -1,11 +1,8 @@
 package main
 
-import (
-	"strings"
-)
+import "strings"
 
 func IsInternal(pkg, subpkg string) bool {
-//	fmt.Printf("\n\n%s\n%s\n-----------\n", pkg, subpkg)
 	//Skip if any is stdlib
 	if IsStdlib(pkg) || IsStdlib(subpkg) {
 		return false
@@ -18,10 +15,10 @@ func IsInternal(pkg, subpkg string) bool {
 
 	// Or it is on same nesting level
 	if i := strings.LastIndex(pkg, "/"); i > 0 {
-//		fmt.Printf("%s\n==================\n\n", pkg[0:i])
 		if strings.HasPrefix(subpkg, pkg[0:i]) {
 			return true
 		}
 	}
+	
 	return false
 }
